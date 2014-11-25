e275a2330c8f586da3894ad4c3df989583d7b487
Garrett <emrickgarrett@gmail.com>
2014-11-25 09:31:59 -0500

    Forgot to remove my red rectangle

--- mycode.js
+++ mycode.js
@@ -2,12 +2,10 @@
 //Use 'c' to draw onto the canvas
 //EXAMPLE
 //
-//c.fillStyle('#FF0000');
-//c.fillRect(0,0,150,75); 
+//c.fillStyle ='#FF0000';
+//c.fillRect(0,0,150,75);
 //
 //will draw a  red rectangle on the screen.
 
 //The canvas is 800 wide, 500 tall.
 
-c.fillStyle ='#FF0000';
-c.fillRect(0,0,150,75);
