1cb58fec5a17e60b3e45d523b79d59543571b849
Garrett <emrickgarrett@gmail.com>
2014-11-25 09:31:00 -0500

    Some formatting changes, now in the center of the page

---
+++ garrettscode.js
@@ -0,0 +1,7 @@
+var Canvas = document.getElementById("myCanvas");
+var	c = Canvas.getContext("2d");
+
+//Let them create their own code in their own file. I may come back and update this code later...
+function init(){
+	//For initialization they may want to do
+}
---
+++ mycode.js
@@ -0,0 +1,13 @@
+//Put your code here.
+//Use 'c' to draw onto the canvas
+//EXAMPLE
+//
+//c.fillStyle('#FF0000');
+//c.fillRect(0,0,150,75); 
+//
+//will draw a  red rectangle on the screen.
+
+//The canvas is 800 wide, 500 tall.
+
+c.fillStyle ='#FF0000';
+c.fillRect(0,0,150,75);
