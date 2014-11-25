285189e0207590b8c58d175ed7ee50a2251aa17b
Garrett <emrickgarrett@gmail.com>
2014-11-25 10:19:45 -0500

    Added a KeyListener and also methods for the kids to use so they don't have to look at the code for it, but it can just work

--- garrettscode.js
+++ garrettscode.js
@@ -6,6 +6,30 @@ var HEIGHT;
 //Let them create their own code in their own file. I may come back and update this code later...
 function init(){
 	//For initialization I may want to do for them.
-	WIDTH = canvas.width;
-	HEIGHT = canvas.height;
+	WIDTH = Canvas.width;
+	HEIGHT = Canvas.height;
+	
+	//Setting Key Listeners that call functions they can use
+	document.onkeydown = function (evt){
+		var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
+		
+		if(keyCode == 13){
+			actionPressed();
+		}
+		if(keyCode == 38 || keyCode == 87){
+			upPressed();
+		}
+		
+		if(keyCode == 39 || keyCode == 68){
+			rightPressed();
+		}
+		
+		if(keyCode == 40 || keyCode == 83){
+			downPressed();
+		}
+		
+		if(keyCode == 65 || keyCode == 37){
+			leftPressed();
+		}
+	}
 }
--- mycode.js
+++ mycode.js
@@ -1,11 +1,44 @@
-//Put your code here.
+//Put your code below here.
 //Use 'c' to draw onto the canvas
 //EXAMPLE
 //
 //c.fillStyle ='#FF0000';
 //c.fillRect(0,0,150,75);
-//
-//will draw a  red rectangle on the screen.
 
-//Use WIDTH and HEIGHT to get how wide and tall the canvas is. Do not change these values.
+
+
+
+
+
+
+
+
+//This is code for when a key is pressed. We will use separate methods for now to
+//Make it easier for you, but that will change later.
+
+//Up Key and 'W'
+function upPressed(){
+	alert("UP");
+}
+
+//Left Key and 'A'
+function leftPressed(){
+	alert("LEFT");
+}
+
+//Right Key and 'D'
+function rightPressed(){
+	alert("RIGHT");
+}
+
+//Down Key and 'S'
+function downPressed(){
+	alert("DOWN");
+}
+
+//Spacebar
+function actionPressed(){
+
+}
+
 
